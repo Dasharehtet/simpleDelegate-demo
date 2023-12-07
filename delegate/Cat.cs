3bf1bf4a45bba51c35941f82b2021c745dfb7b70
Master Yoda <masteryodamaster@outlook.com>
2023-12-07 19:25:23 +0200

    Initial

---
+++ delegate/Cat.cs
@@ -0,0 +1,16 @@
+﻿namespace @delegate;
+
+public class Cat
+{
+    public Cat(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; init; }
+
+    public static void SayMew()
+    {
+        Console.WriteLine("Mew");
+    }
+}
---
+++ delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace @delegate;
+
+public abstract class Program
+{
+    public static void Main()
+    {
+        Action<int, string, bool> action = SomeMethod;
+        action += (x, _, _) => Console.WriteLine(x);
+        action(10, "text", true);
+
+        Func<string, bool, int> func = SomeMethod;
+        //  func += () => 45;
+        Console.WriteLine(func("Test", true));
+
+        Func<int, int, int> func2 = (x, y) => x + y;
+
+        Console.WriteLine(func2(2, 4));
+
+        Action<Cat> catAction = cat => Cat.SayMew();
+        var cat = new Cat("New cat");
+
+        catAction(cat);
+
+        Func<Cat, string> catFunc = cat => cat.Name;
+        Console.WriteLine(catFunc(cat));
+    }
+
+    private static int SomeMethod(string text, bool someBool)
+    {
+        Console.WriteLine(text);
+        return 42; 
+    }
+
+    private static void SomeMethod(int number, string text, bool someBool)
+    {
+        Console.WriteLine($"{number}, {text}, {someBool}");
+    }
+}
